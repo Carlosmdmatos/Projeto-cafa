4d3e57c469efecff7d0df82d186cb8bfa995819a
unknown <carlosmdmatos17@gmail.com>
2022-04-08 17:16:13 -0300

    Fim do Projetocafa 1.0

--- cafa.sql
+++ cafa.sql
@@ -1,8 +1,9 @@
-CREATE TABLE `tabela` (
+CREATE TABLE `usuarios` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `valor` double DEFAULT NULL,
+  `valor` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `date` date DEFAULT NULL,
   `createdAt` datetime NOT NULL,
   `updatedAt` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci
